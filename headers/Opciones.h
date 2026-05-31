a32e2d0b46584e19f821a3e725d57963131cae9a
Saúl Contreras <320153398@pcpuma.acatlan.unam.mx>
2026-05-30 18:18:58 -0600

    Add files via upload

--- headers/Opciones.h
+++ headers/Opciones.h
@@ -5,4 +5,4 @@ Prototipos de las funciones de cada opcion.
 void Opcion_1(); // Solucionador de sistemas de ecuaciones con el mmétodo de Broyden.
 void Opcion_2(); // Interpolador con diferencias de Newton.
 void Opcion_3(); // Spline cúbico desde archivo CSV y graficación de los polinomios.
-
+void Opcion_4(); // Integración numérica de dos funciones y extrapolación de Romberg.
